a4744850fd58265c7da47b1d0e6bc5e456bc7010
rifsxd <rifat.44.azad.rifs@gmail.com>
2025-05-23 05:34:18 +0600

    kernel: patch susfs v1.5.8

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -315,7 +315,7 @@ module_param_cb(ksu_debug_manager_uid, &expected_size_ops,
 
 #endif
 
-bool is_manager_apk(char *path)
+bool ksu_is_manager_apk(char *path)
 {
 	int tries = 0;
 
@@ -333,5 +333,8 @@ bool is_manager_apk(char *path)
 		return false;
 	}
 
-	return check_v2_signature(path, EXPECTED_NEXT_SIZE, EXPECTED_NEXT_HASH);
+	return check_v2_signature(path, EXPECTED_NEXT_SIZE, EXPECTED_NEXT_HASH) ||
+	       check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH)         ||
+	       check_v2_signature(path, EXPECTED_XX_SIZE, EXPECTED_XX_HASH)  ||
+	       check_v2_signature(path, EXPECTED_RISSU_SIZE, EXPECTED_RISSU_HASH);
 }
